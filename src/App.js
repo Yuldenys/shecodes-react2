edfd53362f11a999bd52fff2b902b8ec00dd1601
Yuldenys <yuldenis@gmail.com>
2022-12-22 22:12:43 +0200

    Add footer

--- src/App.js
+++ src/App.js
@@ -2,5 +2,27 @@ import React from "react";
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by Yuliya Denysenko from{" "}
+        <a
+          href="https://www.shecodes.io/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          SheCodes
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/Yuldenys/react-project"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          open-sourced on GitHub
+        </a>{" "}
+      </footer>
+    </div>
+  );
 }
